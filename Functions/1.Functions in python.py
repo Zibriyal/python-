7a2087a90a3cba3217950451b8335f8ab6f7ee94
Zibriyal <124190951+Zibriyal@users.noreply.github.com>
2025-07-08 23:17:37 +0530

    Add files via upload

---
+++ Functions/1.Functions in python.py
@@ -0,0 +1,46 @@
+"""def fun():
+    print("fun() called")
+
+print("Before calling fun()")
+
+fun()
+
+print("After calling fun()")
+"""
+
+"""def fun():
+    print("fun() called")
+
+print("Before calling fun()")
+
+fun()
+fun()
+
+print("After calling fun()")
+"""
+
+"""def printDate(d,m,y):
+    print(d,m,y,sep='-')
+
+print("India became independent on")
+printDate('15','08','1947')
+"""
+
+"""def getDate(d,m,y):
+    return d+'-'+m+'-'+y
+print("India became independent on")
+d=getDate('15','08','1947')
+print(d)
+"""
+
+def greet_msg():
+    print("Hi")
+    print("Welcome to geeksfor geeks")
+
+def exit_msg():
+    print("Please visit again")
+    print("Bye")
+
+greet_msg()
+print("Hope you are enjoying")
+exit_msg()
