3d0a7309a647c592431f10f02561709e0c2875d0
Zibriyal <124190951+Zibriyal@users.noreply.github.com>
2025-07-12 16:40:28 +0530

    Add files via upload

---
+++ Functions/2.How func work.py
@@ -0,0 +1,25 @@
+"""def fun2():
+    print("Inside fun2()")
+
+def fun1():
+    print("Before fun2()")
+    fun2()
+    print("After fun2()")
+
+
+#main code
+print("Before fun1()")
+fun1()
+print("After fun1()")
+"""
+
+def fun():
+    x=5
+    y=10
+    x=x+5
+    print(x,y)
+
+#main code
+
+fun()
+fun()
