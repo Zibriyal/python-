c9b996fe42351b78ce7b7334e0f44ad2b5de13b8
Zibriyal <124190951+Zibriyal@users.noreply.github.com>
2025-08-24 16:30:18 +0530

    Add files via upload

---
+++ String/01.String in python.py
@@ -0,0 +1,24 @@
+print(ord('a'))
+print(ord('A'))
+print(chr(97))
+print(chr(65))
+
+
+
+s='geek'
+print(s)
+print(s[0])
+print(s[-1])
+print(s[1])
+print(s[-2])
+
+# strings are immutable in python
+#s='geek'
+#s[0]='e'
+#print(s)
+
+s="""Hi,
+This is a python course
+Hope you are enjoying it"""
+
+print(s)
