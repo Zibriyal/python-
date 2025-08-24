acd1d331cfe138a750dc3ddadedb18766e130314
Zibriyal <124190951+Zibriyal@users.noreply.github.com>
2025-08-24 22:53:40 +0530

    Add files via upload

---
+++ String/02. Escape-sequence and raw-string.py
@@ -0,0 +1,32 @@
+# \ as escape charater
+
+"""s= 'welcome to geek's course'
+print(s)
+"""
+s='welcome to geek\'s course'
+print(s)
+
+s="Hi,\n Welcome to the course"
+print(s)
+
+
+s1=" a simple \ example"
+#s2=" Backslash at the end \"
+s3=" Backslash at the end \\"
+s4="\\n"
+s5="\\t"
+
+print(s1)
+#print(s2)
+print(s3)
+print(s4)
+print(s5)
+
+
+#Raw string  r and R before string
+
+s1="c:\project\name.py"
+s2=r"c:\project\name.py"
+
+print(s1)
+print(s2)
