02856180f9a87e5ca9e22b7e9899bd6ff5ccac12
Zibriyal <124190951+Zibriyal@users.noreply.github.com>
2025-08-25 22:14:07 +0530

    Add files via upload

---
+++ String/06.String_comparison.py
@@ -0,0 +1,15 @@
+s1='geeksforgeeks'
+s2='ide'
+print(s1<s2)
+print(s1<=s2)
+print(s1>s2)
+print(s1>=s2)
+print(s1==s2)
+print(s1!=s2)
+
+print('-------------------------------------------')
+#More examples
+print('abcd' > 'ABC')
+print('ZAB' > 'ABC')
+print('abc' > 'ABC')
+print('x' > 'abcd')
