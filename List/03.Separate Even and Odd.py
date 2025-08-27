4f06793910e8d52351ae46977663fbfea181418e
Zibriyal <124190951+Zibriyal@users.noreply.github.com>
2025-08-27 16:24:03 +0530

    Add files via upload

---
+++ List/03.Separate Even and Odd.py
@@ -0,0 +1,15 @@
+def getEvenOdd(l):
+    even=[]
+    odd=[]
+    for x in l:
+        if x % 2 == 0:
+            even.append(x)
+        else:
+            odd.append(x)
+    return even, odd
+
+l=[10,12,11,16]
+print(getEvenOdd(l))
+even,odd=getEvenOdd(l)
+print(even)
+print(odd)
