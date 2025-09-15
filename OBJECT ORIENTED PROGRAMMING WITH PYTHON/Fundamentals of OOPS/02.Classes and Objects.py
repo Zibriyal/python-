10b3bc89e0e8f51590cad5fdee70f942e889e44c
Zibriyal <124190951+Zibriyal@users.noreply.github.com>
2025-09-15 10:15:53 +0530

    Add files via upload

---
+++ OBJECT ORIENTED PROGRAMMING WITH PYTHON/Fundamentals of OOPS/02.Classes and Objects.py
@@ -0,0 +1,17 @@
+class complex:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def print(self):
+        print(str(self.real) + " +i " + str(self.imag))
+
+    def add (self, C):
+        self.real = self.real + C.real
+        self.imag = self.imag + C.imag
+
+c1= complex(10,20)
+c1.print()
+c2= complex(20,30  )
+c1.add(c2)
+c1.print()
